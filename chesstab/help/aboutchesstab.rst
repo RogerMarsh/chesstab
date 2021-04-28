ccf0bd2bfa4c08a4b26375610f48414280b82fd4
Roger Marsh <roger.marsh@solentware.co.uk>
2021-04-28 10:30:57 +0000

    chesstab-5.0 published.

--- chesstab/help/aboutchesstab.rst
+++ chesstab/help/aboutchesstab.rst
@@ -6,14 +6,14 @@ ChessTab - About
 ChessTab lists the games on a database and displays the scores of selected games.
 
 
-This is ChessTab-5.0.dev1
+This is ChessTab-5.0
 
 python setup.py install will have installed these packages:
 
 - solentware-base-4.1.4
 - chessql-2.0
-- chesstab-5.0.dev1
-- pgn-read-2.1.dev1
+- chesstab-5.0
+- pgn-read-2.1
 - solentware-grid-2.1.2
 - solentware-misc-1.3
 - uci-net-1.2
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     setup(
         name='chesstab',
-        version='5.0.dev1',
+        version='5.0',
         description='Database for chess games',
         author='Roger Marsh',
         author_email='roger.marsh@solentware.co.uk',
@@ -45,7 +45,7 @@ if __name__ == '__main__':
             'solentware-base==4.1.4',
             'chessql==2.0',
             'solentware-grid==2.1.2',
-            'pgn-read==2.1.dev1',
+            'pgn-read==2.1',
             'solentware-misc==1.3',
             'uci-net==1.2',
             ],
@@ -53,7 +53,7 @@ if __name__ == '__main__':
             'http://solentware.co.uk/files/solentware-base-4.1.4.tar.gz',
             'http://solentware.co.uk/files/chessql-2.0.tar.gz',
             'http://solentware.co.uk/files/solentware-grid-2.1.2.tar.gz',
-            'http://solentware.co.uk/files/pgn-read-2.1.dev1.tar.gz',
+            'http://solentware.co.uk/files/pgn-read-2.1.tar.gz',
             'http://solentware.co.uk/files/solentware-misc-1.3.tar.gz',
             'http://solentware.co.uk/files/uci-net-1.2.tar.gz',
             ],
