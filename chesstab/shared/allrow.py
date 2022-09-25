3350e3b891cd9421a370c9b8daebfe90a53b8c95
Roger Marsh <roger.marsh@solentware.co.uk>
2022-09-25 21:57:30 +0000

    Resolve pylint attribute-defined-outside-init message for allrow.
    Define _init_current_row_background and add it to defining-attr-methods
    list in pylint.conf.
    
    Change chesstab version to 6.0.dev60 in setup.cfg.

--- chesstab/shared/allrow.py
+++ chesstab/shared/allrow.py
@@ -20,10 +20,19 @@ class AllRow:
 
     def grid_row_on_display(self, **kargs):
         """Return grid_row() object with ON_DISPLAY_COLOUR background."""
-        self._current_row_background = ON_DISPLAY_COLOUR
+        self._init_current_row_background()
         return self.grid_row(background=ON_DISPLAY_COLOUR, **kargs)
 
     def set_background_on_display(self, widgets):
         """Set background to ON_DISPLAY_COLOUR on all widgets."""
-        self._current_row_background = ON_DISPLAY_COLOUR
+        self._init_current_row_background()
         self.set_background(widgets, self._current_row_background)
+
+    def _init_current_row_background(self):
+        """Initialize _current_row_background to ON_DISPLAY_COLOUR.
+
+        This method exists to be added to the defining-attr-methods list
+        in pylint configuration.
+        """
+        assert hasattr(self, "_current_row_background")
+        self._current_row_background = ON_DISPLAY_COLOUR
